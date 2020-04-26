0244654c9ef46cede87ffa177bc4b1472eac00db
Kurban <Mr.gustav009@gmail.com>
2020-04-26 17:27:50 +0300

    Header component

---
+++ src/components/header/header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './header.scss';
+
+const Header = () => {
+    return (
+        <header>
+            <div className="header">
+                <div className="header-title">
+                    <h1>The best bakeries</h1>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
